22c13748400dcd923030a5ac9a383e5fe79cfec2
Sergey Fokin <memonanofsa@gmail.com>
2025-12-11 11:14:44 +0500

    Added skinport credentials into configuration of the server

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,6 +10,12 @@ import (
 type Config struct {
 	ServerPort  string
 	DatabaseURL string
+
+	Skinport struct {
+		APIURL   string
+		ClientID string
+		APIKey   string
+	}
 }
 
 func Load() (*Config, error) {
@@ -26,8 +32,32 @@ func Load() (*Config, error) {
 		return nil, fmt.Errorf("DATABASE_URL must be set")
 	}
 
+	skinportAPIURL := os.Getenv("SKINPORT_API_URL")
+	if skinportAPIURL == "" {
+		return nil, fmt.Errorf("SKINPORT_API_URL must be set")
+	}
+
+	skinportClientID := os.Getenv("SKINPORT_CLIENT_ID")
+	if skinportClientID == "" {
+		return nil, fmt.Errorf("SKINPORT_CLIENT_ID must be set")
+	}
+
+	skinportAPIKey := os.Getenv("SKINPORT_API_KEY")
+	if skinportAPIKey == "" {
+		return nil, fmt.Errorf("SKINPORT_API_KEY must be set")
+	}
+
 	return &Config{
 		ServerPort:  serverPort,
 		DatabaseURL: databaseURL,
+		Skinport: struct {
+			APIURL   string
+			ClientID string
+			APIKey   string
+		}{
+			APIURL:   skinportAPIURL,
+			ClientID: skinportClientID,
+			APIKey:   skinportAPIKey,
+		},
 	}, nil
 }
